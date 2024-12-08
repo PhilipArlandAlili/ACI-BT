2bf1ee372072cb7fa58a6ab9665c59a66ecb0f54
Philip Man <202080139@psu.palawan.edu.ph>
2024-12-08 21:59:29 +0800

    modified cert1

--- forms/certificate_of_employability.php
+++ forms/certificate_of_employability.php
@@ -1,31 +1,38 @@
 <form action="#" method="POST" id="form">
-    <!-- Birthday input -->
+    
     <label for="first_name">First Name:</label>
     <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Ex. Juan" required><br>
 
-    <div class="form-row d-flex justify-content-between">
-        <div class="form-group col-sm-2">
-            <label for="middle_initial">M.I. :</label>
-            <input type="text" class="form-control" id="middle_initial" name="middle_initial" placeholder="Ex. M"
-                maxlength="1" pattern="[A-Za-z]" style="text-transform: uppercase;" required>
-        </div>
-        <div class="form-group col-sm-6">
-            <label for="last_name">Last Name:</label>
-            <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Ex. Dela Cruz"
-                required><br>
-        </div>
-        <div class="form-group col-sm-2">
-            <label for="suffix">Suffix:</label>
-            <select class="text-left form-control" name="suffix" id="suffix">
-                <option value="">N/A</option>
-                <option value="Jr">Jr</option>
-                <option value="Sr">Sr</option>
-                <option value="I">I</option>
-                <option value="II">II</option>
-                <option value="III">III</option>
-            </select>
-        </div>
-    </div>
+    <label for="middle_name">Middle Name:</label>
+    <input type="text" class="form-control" id="middle_name" name="middle_name" placeholder="Ex. Mandaragat"
+        required><br>
+
+    <label for="last_name">Last Name:</label>
+    <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Ex. Dela Cruz" required><br>
+
+    <label for="suffix">Suffix:</label>
+    <select class="text-left form-control" name="suffix" id="suffix">
+        <option value="">N/A</option>
+        <option value="Jr">Jr</option>
+        <option value="Sr">Sr</option>
+        <option value="I">I</option>
+        <option value="II">II</option>
+        <option value="III">III</option>
+    </select><br>
+
+    <label for="puroks">Purok:</label>
+    <select class="text-left form-control" name="purok" id="purok" required>
+        <option value="">--Select Purok--</option>
+        <option value="Centro">Centro</option>
+        <option value="Hurawan">Huwaran</option>
+        <option value="Kaakbayan">Kaakbayan</option>
+        <option value="New Princesa">New Princesa</option>
+        <option value="San Franciso I">San Franciso I</option>
+        <option value="San Franciso II">San Franciso II</option>
+        <option value="Sandiwa">Sandiwa</option>
+        <option value="Trece">Trece</option>
+        <option value="Uha">UHA</option>
+    </select><br>
 
     <!-- Birthday input -->
     <label for="birthday">Birthday:</label>
--- forms/certificate_of_income.php
+++ forms/certificate_of_income.php
@@ -1,43 +1,41 @@
 <form action="#" method="post" id="form">
-    <label for="">First Name:</label>
-    <input type="text" class="form-control" name="first_name" placeholder="Ex. Juan"><br>
 
-    <label for="">Middle Initial:</label>
-    <input type="text" class="form-control" name="middle_initial" placeholder="Ex. J"><br>
+    <label for="first_name">First Name:</label>
+    <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Ex. Juan" required><br>
 
-    <label for="">Last Name:</label>
-    <input type="text" class="form-control" name="last_name" placeholder="Ex. J"><br>
+    <label for="middle_name">Middle Name:</label>
+    <input type="text" class="form-control" id="middle_name" name="middle_name" placeholder="Ex. Mandaragat"
+        required><br>
 
+    <label for="last_name">Last Name:</label>
+    <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Ex. Dela Cruz" required><br>
 
-    <label for="">Suffix:</label>
-    <!-- <input type="text" class="form-control" name="suffix" placeholder=""><br> -->
-    <select class=" text-left" style="width: 8%;" name="suffix" id="suffixs">
+    <label for="suffix">Suffix:</label>
+    <select class="text-left form-control" name="suffix" id="suffix">
         <option value="">N/A</option>
         <option value="Jr">Jr</option>
         <option value="Sr">Sr</option>
         <option value="I">I</option>
         <option value="II">II</option>
         <option value="III">III</option>
-    </select><br><br>
+    </select><br>
 
-    <label for="">Purok:</label><br>
-    <select name="puroks" id="puroks" onchange="update()">
+    <label for="puroks">Purok:</label>
+    <select class="text-left form-control" name="purok" id="purok" required>
+        <option value="">--Select Purok--</option>
         <option value="Centro">Centro</option>
         <option value="Hurawan">Huwaran</option>
         <option value="Kaakbayan">Kaakbayan</option>
-        <option value="New Princesa"> New Princesa</option>
+        <option value="New Princesa">New Princesa</option>
         <option value="San Franciso I">San Franciso I</option>
         <option value="San Franciso II">San Franciso II</option>
         <option value="Sandiwa">Sandiwa</option>
         <option value="Trece">Trece</option>
         <option value="Uha">UHA</option>
-    </select>
-    <br>
-    <label for="">Amount (In Numeric Form)</label>
-    <input type="number" name="amount" class="form-control" maxlength="10"><br>
+    </select><br>
 
-    <!-- <label for="">Issued Date:</label>
-                  <input type="date" class="form-control"> -->
+    <label for="">Amount (In Numeric Form)</label>
+    <input type="number" name="amount" class="form-control" maxlength="10" placeholder="Ex. 123456789" required><br>
 
     <label for="">Duty Officer Full Name:</label>
     <input type="text" class="form-control">
