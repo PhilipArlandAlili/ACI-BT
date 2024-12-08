00629f6c56a527a12cb135c8c772d682c9aea66f
Kaaru <202080135@psu.palawan.edu.ph>
2024-12-08 18:57:00 +0800

    updated db and view_reports

--- includes/db.php
+++ includes/db.php
@@ -3,8 +3,9 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
+//$password = "Alabastro_8";
 // $database = "acibt_db";
-$database = "aci_bt_final";
+$database = "aci_bt";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
