8fd2a78dc31f3480b4b6570877547d9841354c6e
Philip Man <202080139@psu.palawan.edu.ph>
2024-12-09 11:14:24 +0800

    add transfer

--- forms/transfer_of_residency.php
+++ forms/transfer_of_residency.php
@@ -1,18 +1,17 @@
-<form action="#" method="post" id="form">
+<form action="#" method="post" id="transferForm">
 
-    <label for="">First Name:</label>
-    <input type="text" class="form-control" name="first_name" placeholder="Ex. Juan"><br>
+    <label for="first_name">First Name:</label>
+    <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Ex. Juan" required><br>
 
-    <label for="">Middle Initial:</label>
-    <input type="text" class="form-control" name="middle_name" placeholder="Ex. J"><br>
+    <label for="middle_name">Middle Name:</label>
+    <input type="text" class="form-control" id="middle_name" name="middle_name" placeholder="Ex. Mandaragat"
+        required><br>
 
-    <label for="">Last Name:</label>
-    <input type="text" class="form-control" name="last_name" placeholder="Ex. Dela Cruz"><br>
+    <label for="last_name">Last Name:</label>
+    <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Ex. Dela Cruz" required><br>
 
-
-    <label for="">Suffix:</label>
-    <!-- <input type="text" class="form-control" name="suffix" placeholder=""><br> -->
-    <select class="form-control text-left" name="suffix" id="suffixs">
+    <label for="suffix">Suffix:</label>
+    <select class="text-left form-control" name="suffix" id="suffix">
         <option value="">N/A</option>
         <option value="Jr">Jr</option>
         <option value="Sr">Sr</option>
@@ -21,12 +20,13 @@
         <option value="III">III</option>
     </select><br>
 
-    <label for="">Purok:</label>
-    <select name="puroks" class="form-control" id="puroks" onchange="update()">
+    <label for="purok">Purok:</label>
+    <select class="text-left form-control" name="purok" id="purok" required>
+        <option value="">--Select Purok--</option>
         <option value="Centro">Centro</option>
         <option value="Hurawan">Huwaran</option>
         <option value="Kaakbayan">Kaakbayan</option>
-        <option value="New Princesa"> New Princesa</option>
+        <option value="New Princesa">New Princesa</option>
         <option value="San Franciso I">San Franciso I</option>
         <option value="San Franciso II">San Franciso II</option>
         <option value="Sandiwa">Sandiwa</option>
@@ -35,32 +35,29 @@
     </select><br>
 
     <label for="">Current Address:</label>
-    <input type="text" class="form-control" name="current_address" placeholder="Ex. Previous Address"><br>
+    <input type="text" class="form-control" name="current_address" placeholder="Ex. Previous Address" required><br>
 
     <label for="">Previous Address:</label>
-    <input type="text" class="form-control" name="previous_address" placeholder="Ex. Previous Address"><br>
+    <input type="text" class="form-control" name="previous_address" placeholder="Ex. Previous Address" required><br>
 
     <label for="">Nationality</label>
-    <input type="text" class="form-control" name="nationality" placeholder="Filipino"><br>
+    <input type="text" class="form-control" name="nationality" placeholder="Filipino" required><br>
 
     <label for="">Civil Status:</label>
-    <select class="form-control" onchange="update()" name="civil_status" id="stats">
+    <select class="form-control" onchange="update()" name="civil_status" id="stats" required>
+        <option value="">--Select Civil Status--</option>
+        <option value="Single">Single</option>
         <option value="Married">Married</option>
         <option value="Widow">Widow</option>
-        <option value="Single">Single</option>
     </select><br>
 
     <label for="">Purpose:</label>
     <input type="text" name="purpose" class="form-control" id="" cols="30" rows="10"
-        placeholder="Ex. Moving to another country"></input>
+        placeholder="Ex. Moving to another country" required><br><hr>
 
-    <input type="date" name="issueddate" style="display:none; position:absolute;">
-    <hr>
-
-    <div class="print" style="text-align: right;">
-        <button type="button" class="btn btn-primary w-25" data-bs-toggle="modal"
-            data-bs-target="#transferResidency">Print</button>
-        <div class="modal fade" id="transferResidency" tabindex="-1" style="display: none;" aria-hidden="true">
+        <div class="transferPrint" style="text-align: right;">
+        <button type="button" class="btn btn-primary w-25" id="transferPrintBtn">Print</button>
+        <div class="modal fade" id="transferModal" tabindex="-1" style="display: none;" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content text-center">
                     <div class="modal-header">
@@ -70,13 +67,28 @@
                         <h2 style="font-weight: bold;">Confirm if all the data is correct?</h2>
                     </div>
                     <div class="modal-footer d-flex justify-content-around">
-                        <button class="w-25 btn btn-primary" name="transfer_of_residency" onclick="printIframe()" id="coco"
-                            type="submit">Yes</button>
+                        <button class="w-25 btn btn-primary" name="transfer_of_residency" onclick="printIframe()" id="confirmPrint" type="submit">Yes</button>
                         <button type="button" class="w-25 btn btn-danger" data-bs-dismiss="modal">No</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
 </form>
+
+<script>
+    // Event listener for Business Permit Renewal print button
+    document.getElementById("transferPrintBtn").addEventListener("click", function () {
+        let form = document.getElementById("transferForm");  // Use the unique form ID
+
+        // Validate the form
+        if (form.checkValidity()) {
+            // If the form is valid, show the Business Permit Renewal modal
+            let transferModal = new bootstrap.Modal(document.getElementById("transferModal"));
+            transferModal.show();
+        } else {
+            // If the form is not valid, show the built-in validation messages
+            form.reportValidity();
+        }
+    });
+</script>
