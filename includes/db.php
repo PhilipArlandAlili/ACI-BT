1d6866ff645d1b4668b4d65c88a61b6a2879be67
Kristian Marc Anthony Mendoza Ng <142019535+KMAN321@users.noreply.github.com>
2024-12-10 23:21:54 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -8,4 +8,10 @@ $password = "root";
 // $database = "aci_bt_final";
 $database = "aci_bt";
 
+// $servername = "153.92.15.40";
+// $username = "u449496944_root";
+// $password = "9X#+F81gZ&z";
+// $database = "u449496944_aci_bt";
+
+
 $conn = new mysqli($servername, $username, $password, $database);
