65dda9de0d81fc3dd5f60e337a38afa7c6253343
FrnzMgl <141212133+FrnzMgl@users.noreply.github.com>
2024-12-10 14:10:08 +0800

    employability done

--- forms/certificate_of_employability.php
+++ forms/certificate_of_employability.php
@@ -1,13 +1,13 @@
 <form action="#" method="POST" id="employabilityForm">
 
-    <label for="first_name">First Name:</label>
-    <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Ex. Juan" required><br>
+<label for="">First Name:</label>
+    <input type="text" class="form-control" name="first_name" maxlength="50"  oninput="this.value = this.value.replace(/[^A-Za-z ]/g, '').replace(/^([^.]*)\.(.*)\./, '$1.$2');updateText();" required placeholder="Ex. Juan"><br>
 
-    <label for="middle_name">Middle Name:</label>
-    <input type="text" class="form-control" id="middle_name" name="middle_name" placeholder="Ex. Mandaragat" required><br>
+    <label for="">Middle Name:</label>
+    <input type="text" class="form-control" name="middle_name" maxlength="50"  oninput="this.value = this.value.replace(/[^A-Za-zs]/g, '').replace(/^([^.]*)\.(.*)\./, '$1.$2');updateText();" required  placeholder="Ex. J"><br>
 
-    <label for="last_name">Last Name:</label>
-    <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Ex. Dela Cruz" required><br>
+    <label for="">Last Name:</label>
+    <input type="text" class="form-control" name="last_name" maxlength="50"  oninput="this.value = this.value.replace(/[^A-Za-z']/g, '').replace(/^([^.]*)\.(.*)\./, '$1.$2');updateText();" required  placeholder="Ex. Dela Cruz"><br>
 
     <label for="suffix">Suffix:</label>
     <select class="text-left form-control" name="suffix" id="suffix">
@@ -35,7 +35,7 @@
 
     <!-- Birthday input -->
     <label for="birthday">Birthday:</label>
-    <input type="date" class="form-control" id="birthdate" name="birthdate" required><br>
+    <input type="date" class="form-control" onchange="validatebday(this);updateText();" id="birthdate" name="birthdate" required><br>
 
     <!-- Age input (readonly) -->
     <!-- <label for="age">Age:</label>
