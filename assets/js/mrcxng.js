901e86f89f3b51a9291704f198914458c53718f1
Philip Man <202080139@psu.palawan.edu.ph>
2024-12-13 06:03:48 +0800

    bigat ni carl

--- assets/js/mrcxng.js
+++ assets/js/mrcxng.js
@@ -60,7 +60,7 @@ function updateText() {
         // From HTML
         var pfirst_name = iframeDocument.getElementById('first_name');
         var pmiddle_name = iframeDocument.getElementById('middle_name');
-        var cid= iframeDocument.getElementById('last_name');
+        var cid = iframeDocument.getElementById('last_name');
         var psuffix = iframeDocument.getElementById('suffix');
         var ppurok = iframeDocument.getElementById('purok');
         var pbirthplace = iframeDocument.getElementById('birthplace');
@@ -229,15 +229,15 @@ function updateText() {
         pcocbirthdate_female_day.innerText = cocday_female + ", ";
         pcocbirthdate_female_year.innerText = cocyear_female;
         pcocpurok.innerText = cocpurok.value;
-        
+
         var coctoday = new Date();
         var coctyear = coctoday.getFullYear();
 
-        var years_married = coctyear-cocyear_dom;
+        var years_married = coctyear - cocyear_dom;
 
         pcocdate_of_marriage.innerText = years_married;
 
-        if (years_married == 1){
+        if (years_married == 1) {
             pcocdom_y.innerText = 'Year';
         } else {
             pcocdom_y.innerText = 'Years';
@@ -304,8 +304,41 @@ function updateText() {
 
     } else if (certificate_type.value == 'lot_ownership') {
 
-    } else if (certificate_type.value == 'transfer_of)residency') {
+    } else if (certificate_type.value == 'transfer_of_residency') {
+        var trfirst_name = document.getElementById('trfirst_name');
+        var trmiddle_name = document.getElementById('trmiddle_name');
+        var trlast_name = document.getElementById('trlast_name');
+        var trsuffix = document.getElementById('trsuffix');
+        var trnationality = document.getElementById('trnationality');
+        var trcivil_status = document.getElementById('trcivil_status');
+        var trpurok = document.getElementById('trpurok');
+        var trcurrent_address = iframeDocument.getElementById('trcurrent_address');
+        var trprevious_address = document.getElementById('trprevious_address');
+        var trpurpose = document.getElementById('trpurpose');
 
+        // From HTML
+        var ptrfirst_name = iframeDocument.getElementById('trfirst_name');
+        var ptrmiddle_name = iframeDocument.getElementById('trmiddle_name');
+        var ptrlast_name = iframeDocument.getElementById('trlast_name');
+        var ptrsuffix = iframeDocument.getElementById('trsuffix');
+        var ptrnationality = iframeDocument.getElementById('trnationality');
+        var ptrcivil_status = iframeDocument.getElementById('trcivil_status');
+        var ptrpurok = iframeDocument.getElementById('trpurok');
+        var ptrcurrent_address = iframeDocument.getElementById('trcurrent_address');
+        var ptrprevious_address = iframeDocument.getElementById('trprevious_address');
+        var ptrpurpose = iframeDocument.getElementById('trpurpose');
+
+
+        ptrfirst_name.innerText = trfirst_name.value;
+        ptrmiddle_name.innerText = trmiddle_name.value;
+        ptrlast_name.innerText = trlast_name.value;
+        ptrsuffix.innerText = trsuffix.value;
+        ptrnationality.innerText = trnationality.value;
+        ptrcivil_status.innerText = trcivil_status.value;
+        ptrpurok.innerText = trpurok.value;
+        ptrcurrent_address.innerText = trpurok.value;
+        ptrprevious_address.innerText = trprevious_address.value;
+        ptrpurpose.innerText = trpurpose.value;
     }
 
 }
