c9b872f0d623963ce78c4f94a1dfc557840c2b60
Philip Man <202080139@psu.palawan.edu.ph>
2024-12-12 22:40:20 +0800

    death cert, ownership cert done

--- forms/aacert_changes.php
+++ forms/aacert_changes.php
@@ -1,12 +1,44 @@
-Certificate of transfer
-Yung Purok and Current dapat same lang
-Civil Status and Purpose di nagrereflect
-Try kung maayos pagkakasunod sunod sa label-cert_input
-        Name
-        Nationality
-        Civil Status
-        Purok
-        Previous
-        Purpose
-Sabi lang kung ilipat lipat label
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+</head>
+
+<body>
+
+        <div class="transfer">
+                <h1>Certificate of Transfer</h1>
+                <div class="to-do">
+                        Yung Purok and Current dapat same lang
+                        Civil Status and Purpose di nagrereflect
+                        Try kung maayos pagkakasunod sunod sa label-cert_input
+                        Name
+                        Nationality
+                        Civil Status
+                        Purok
+                        Previous
+                        Purpose
+                        Sabi lang kung ilipat lipat label
+                </div>
+        </div>
+
+        <div class="lot-ownership">
+                <h1>Lot Ownership</h1>
+                <div class="to-do">
+                        Lot location not working
+                </div>
+        </div>
+
+        <div class="death-certificate">
+                <h1>Death Certificate</h1>
+                <div class="to-do">
+                        Birthday not working (Age nakalagay sa cert)
+                        Suffix not working
+                        Relationship dead not working
+                </div>
+        </div>
+
+
+</body>
+
+</html>
